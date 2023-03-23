1162471cc227f8eca99a2f2582916d25ecb10056
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-03-23 19:28:59 -0400

    adding reciprocal changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
+    has_many :reviews
     has_many :recipes, through: :reviews
  
     validates :username, length: { minimum: 3 }, uniqueness: true
