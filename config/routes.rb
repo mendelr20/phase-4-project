928903e9b6ecfb4ec77a09cf806a2716c0227350
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-04-02 09:15:17 -0400

    upating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get "/users", to: "users#index"
+  resources :users, only: [:index, :create, :show]
+ 
+  resources :recipes, only: [:index, :create]
+  resources :reviews, only: [:create, :update, :destroy]
+  resources :users, only: [:index]
+ 
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  get "/recipes", to: "recipes#index"
-  post "/recipes", to: "recipes#create"
-  delete '/reviews/:id', to: 'reviews#destroy'
-  post "/reviews", to: "reviews#create"
-  patch "/reviews/:id", to: "reviews#update"
-  # Routing logic: fallback requests for React Router.
+
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
