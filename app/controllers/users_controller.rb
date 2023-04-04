1aae995fb190be83421362b187176c2258c846cc
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-04-04 10:28:24 -0400

    fixing user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.permit(:username, :password, :password_confirmation)
   end
 end
